aca4807aceaca74d98f47f7994a796b85675ec44
Deivison Jimenez <deivi.jimenez@gmail.com>
2022-10-28 09:12:06 -0400

    Agregar archivos de proyecto.

---
+++ Seccionejercicio2/Models/Formulario/formularioViewModels.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Seccionejercicio2.Models.Formulario
+{
+    public class formularioViewModels
+    {
+        [MaxLength(6)]
+        [RegularExpression("[MXM] {3}-[0-9]{3}")]
+        public string Usuario { get; set; }
+        [StringLength(15, MinimumLength = 5)]
+        public string nombre { get; set; }
+        [StringLength(10, MinimumLength = 5)]
+        public string cargo { get; set; }
+        [Phone]
+        //[RegularExpression("[\+-]?")]
+        public string telefono { get; set; }
+
+        [EmailAddress]
+        public string correore { get; set; }
+
+    }
+}
---
+++ Seccionejercicio2/Scripts/formulario.js
@@ -0,0 +1,10 @@
+﻿'use strict'
+
+alert('si estoy cargando')
+
+var form = document.forms['form'];
+form.onsubmit = funcion(e){
+    e.preventDefault();
+    var select = document.form.tipo.value;
+    console.log(select);
+}
